f07782a9d10a21d895b09c21e1f5cadbcffeaa79
Howard Kim <howardkim820@gmail.com>
2018-08-09 15:39:39 -0700

    finished todo list

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,49 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import TodoList from './todo_list'
+import React, {Component} from 'react';
+import TodoList from './todo_list';
+import AddItem from './add_item';
+import listData from '../data/todo';
+//need to change to a class component
 
-const App = () => (
-    <div className="container">
-        <h1 className="center">To Do List</h1>
-        <TodoList />
-    </div>
-);
+class App extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            items: [],
+        }
+    }
+    componentDidMount(){
+        this.getListData();
+    }
+    
+    addItem = (item) => {
+        item._id = new Date().getTime();
+        this.setState({
+            //item goes on top because of this order
+            items: [item, ...this.state.items]
+        })
+    }
+
+    getListData(){
+        //This is where you would call the server for your data
+
+        this.setState({
+            items: listData
+        })
+    }
+    render(){
+        console.log(this.state.items)
+        return (
+        <div className="container">
+                <h1 className="center">To Do List</h1>
+                
+                <AddItem add ={this.addItem}/>
+                <TodoList list={this.state.items}/>
+            </div>
+        );
+    }
+}
 
 export default App;
+
