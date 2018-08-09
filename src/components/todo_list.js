4dd618252fdf27be5fb89f77bb7a5da2944511b3
Howard Kim <howardkim820@gmail.com>
2018-08-09 14:25:52 -0700

    displayed list

---
+++ src/components/todo_list.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import listData from '../data/todo';
+
+class TodoList extends Component{
+    render(){
+        const listElements = listData.map(item =>{
+            return <li className="collection-item" key={item._id}>{item.title}</li>
+        })
+        return (
+            <ul className="collection">
+                {listElements}
+            </ul>
+        )
+    }
+}
+
+export default TodoList;
